bceb7093dab046573a4eefd0aab4879cb3b349a9
xuxulia <juliabeatriz.souza0704@gmail.com>
2021-05-06 16:01:56 -0300

    Laço de repetição WHILE

---
+++ src/br/com/senai/LacoDeRepeticaoWhile.java
@@ -0,0 +1,18 @@
+package br.com.senai;
+
+public class LacoDeRepeticaoWhile {
+
+	public static void main(String[] args) {
+		int valor = 0;
+		System.out.println("Valor: " + valor);
+		
+		while(valor <= 25) {
+			System.out.println("-> Valor: " + valor);
+			valor += 1;
+		}
+		
+		System.out.println("Agora o valor é: " + valor);
+
+	}
+
+}
